c519c710deb573d2b60bf48a740bea8c9222cc20
Christine Cha <hello@christinecha.com>
2020-11-12 23:49:20 -0500

    Leaderboard yeey

---
+++ client/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <main>
+      <h1>LEADERBOARD</h1>
+      <p>
+        jk y'all losers. go to sleep
+      </p>
+    </main>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
