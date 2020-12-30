26c1a68ef344c5486e94a4993cd8a4120b18fdce
Christine Cha <hello@christinecha.com>
2020-12-30 00:50:50 -0500

    Oops css.

--- client/app/Leaderboard.js
+++ client/app/Leaderboard.js
@@ -4,7 +4,9 @@ import qs from 'qs'
 import League from './League'
 import { getGuild } from '../api'
 
-const { guildId, teamSize } = qs.parse(window.location.search, { ignoreQueryPrefix: true })
+const { guildId, teamSize } = qs.parse(window.location.search, {
+  ignoreQueryPrefix: true,
+})
 
 const LeagueTab = ({ teamSize, onSelect, active }) => {
   return (
@@ -21,7 +23,7 @@ const LeagueTab = ({ teamSize, onSelect, active }) => {
         margin-right: 20px;
         cursor: pointer;
         ${active ? 'border-bottom: 1px solid #90ff00;' : ''};
-        
+
         &:focus {
           outline: 1px solid #90ff00;
           outline-offset: 4px;
@@ -34,23 +36,15 @@ const LeagueTab = ({ teamSize, onSelect, active }) => {
 }
 
 const Loading = () => {
-  return (
-    <div>
-      Loading...
-    </div>
-  )
+  return <div>Loading...</div>
 }
 
 const NotFound = () => {
-  return (
-    <div>
-      Leaderboard not found. :'(
-    </div>
-  )
+  return <div>Leaderboard not found. :'(</div>
 }
 
 export const Leaderboard = () => {
-  const initialSize = [1, 2, 3, 4].find(t => t === parseInt(teamSize)) || 3
+  const initialSize = [1, 2, 3, 4].find((t) => t === parseInt(teamSize)) || 3
   const [show4s, setShow4s] = useState(false)
   const [size, setSize] = useState(initialSize)
   const [loading, setLoading] = useState(true)
@@ -72,44 +66,50 @@ export const Leaderboard = () => {
   if (!guild) return <Loading />
 
   return (
-    <div>
-      <section className={css`
-        display: flex;
-      `}>
-        <h1 className={css`
-          flex: 1;
-          text-transform: uppercase;
-        `}>
-          {guild.name} Leaderboard
-        </h1>
-        <span
+    <div data-row>
+      <div data-col="12">
+        <section
           className={css`
             display: flex;
-            align-items: center;
-            cursor: pointer;
-            user-select: none;
           `}
-          onClick={set4s}
         >
-          {show4s ? '👻' : '🏆'}
-        </span>
-      </section>
+          <h1
+            className={css`
+              flex: 1;
+              text-transform: uppercase;
+            `}
+          >
+            {guild.name} Leaderboard
+          </h1>
+          <span
+            className={css`
+              display: flex;
+              align-items: center;
+              cursor: pointer;
+              user-select: none;
+            `}
+            onClick={set4s}
+          >
+            {show4s ? '👻' : '🏆'}
+          </span>
+        </section>
 
-      {!show4s && (
-        <>
-          <LeagueTab onSelect={setSize} teamSize={1} active={size === 1} />
-          <LeagueTab onSelect={setSize} teamSize={2} active={size === 2} />
-          <LeagueTab onSelect={setSize} teamSize={3} active={size === 3} />
-        </>
-      )}
+        {!show4s && (
+          <>
+            <LeagueTab onSelect={setSize} teamSize={1} active={size === 1} />
+            <LeagueTab onSelect={setSize} teamSize={2} active={size === 2} />
+            <LeagueTab onSelect={setSize} teamSize={3} active={size === 3} />
+          </>
+        )}
 
-      {show4s && (
-        <LeagueTab onSelect={setSize} teamSize={4} active={size === 4} />
-      )}
+        {show4s && (
+          <LeagueTab onSelect={setSize} teamSize={4} active={size === 4} />
+        )}
 
-      <br />
-      <br />
-      <League teamSize={size} guildId={guildId} />
+        <br />
+        <br />
+        <League teamSize={size} guildId={guildId} />
+      </div>
     </div>
   )
 }
