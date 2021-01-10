52235b02ac7c78b30b15e2354335f7cb715271d7
Christine Cha <hello@christinecha.com>
2021-01-10 13:27:43 -0500

    Clean up leave test

--- server/bot/command-leave.test.js
+++ server/bot/command-leave.test.js
@@ -1,40 +1,20 @@
 // Start mock server!
 require('./index')
-const firebase = require('@firebase/rules-unit-testing')
 const leagues = require('../data/leagues')
 const { discord } = require('../data/util/discord')
 const messages = require('./messages')
 const ERRORS = require('../constants/ERRORS')
 const { league1s, league2s, league3s } = require('../test/league')
-const { guild } = require('../test/guild')
+const { cleanDatabase, triggerMessage } = require('../test/util')
 const BOT_ID = process.env.BOT_ID
 
-let send, msg, react
 const users = ['suhan', 'tandk', 'caudex']
 
 beforeAll(async (done) => {
-  await firebase.clearFirestoreData({
-    projectId: process.env.GCLOUD_PROJECT,
-  })
-
+  await cleanDatabase()
   done()
 })
-
 beforeEach(async (done) => {
-  react = jest.fn()
-  send = jest.fn(() =>
-    Promise.resolve({
-      react,
-      awaitReactions: () => Promise.resolve(),
-    })
-  )
-  msg = (userId, content) => ({
-    content,
-    author: { id: userId },
-    guild,
-    channel: { send, id: 'test' },
-  })
-
   const queue = {}
   users.forEach((u, i) => (queue[u] = i + 1000))
 
@@ -45,16 +25,24 @@ beforeEach(async (done) => {
 })
 
 afterEach(async (done) => {
-  await leagues.delete(league1s.id)
-  await leagues.delete(league2s.id)
-  await leagues.delete(league3s.id)
+  await cleanDatabase()
   done()
 })
 
 test('@LeagueBot leave <league>', async (done) => {
-  await discord.trigger('message', msg(users[0], `<@!${BOT_ID}> leave 1s`))
-  await discord.trigger('message', msg(users[0], `<@!${BOT_ID}> leave 2s`))
-  await discord.trigger('message', msg(users[0], `<@!${BOT_ID}> leave 3s`))
+  const { send } = await discord.channels.fetch('test')
+  await triggerMessage({
+    userId: users[0],
+    content: `<@!${BOT_ID}> leave 1s`,
+  })
+  await triggerMessage({
+    userId: users[0],
+    content: `<@!${BOT_ID}> leave 2s`,
+  })
+  await triggerMessage({
+    userId: users[0],
+    content: `<@!${BOT_ID}> leave 3s`,
+  })
 
   // Confirmations should be sent.
   expect(send).toHaveBeenNthCalledWith(
@@ -78,10 +66,18 @@ test('@LeagueBot leave <league>', async (done) => {
   expect(Object.keys(league1.queue)).toStrictEqual([users[1], users[2]])
   expect(Object.keys(league2.queue)).toStrictEqual([users[1], users[2]])
   expect(Object.keys(league3.queue)).toStrictEqual([users[1], users[2]])
-
-  await discord.trigger('message', msg(users[0], `<@!${BOT_ID}> leave 1s`))
-  await discord.trigger('message', msg(users[0], `<@!${BOT_ID}> leave 2s`))
-  await discord.trigger('message', msg(users[0], `<@!${BOT_ID}> leave 3s`))
+  await triggerMessage({
+    userId: users[0],
+    content: `<@!${BOT_ID}> leave 1s`,
+  })
+  await triggerMessage({
+    userId: users[0],
+    content: `<@!${BOT_ID}> leave 2s`,
+  })
+  await triggerMessage({
+    userId: users[0],
+    content: `<@!${BOT_ID}> leave 3s`,
+  })
 
   // The same user may not leave again.
   expect(send).toHaveBeenNthCalledWith(4, ERRORS.QUEUE_NO_SUCH_USER)
