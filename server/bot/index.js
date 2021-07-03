53fb4640384abf56ea09785c2f487ce46b08d192
Christine Cha <hello@christinecha.com>
2021-07-02 23:01:34 -0600

    Better logs

--- server/bot/index.js
+++ server/bot/index.js
@@ -19,58 +19,58 @@ discord.on('warn', (info) => {
 })
 
 discord.on('message', async (message) => {
-  let prefixed = false
-  const parts = message.content.split(/\s+/)
+  try {
+    let prefixed = false
+    const parts = message.content.split(/\s+/)
 
-  if (!parts[0].match(BOT_ID)) {
-    parts.unshift(parts[0][0]) // save prefix
-    parts[1] = parts[1].substr(1)
-    prefixed = true
-  }
-
-  const [prefix, _command, ...args] = parts
-  const context = message
-  const command = Object.keys(COMMAND_NAME).find((name) => {
-    const config = COMMANDS[name]
-    const match = (_command || '').toLowerCase()
-    return (
-      config.command === match ||
-      (config.aliases && config.aliases.includes(match))
-    )
-  })
-
-  if (!command) {
-    if (!prefixed) {
-      message.channel.send(
-        'Sorry, I didn\'t understand that command. Try "@LeagueBot help" for more info.'
-      )
+    if (!parts[0].match(BOT_ID)) {
+      parts.unshift(parts[0][0]) // save prefix
+      parts[1] = parts[1].substr(1)
+      prefixed = true
     }
-    return
-  }
 
-  const guild = await guilds.get(message.guild.id)
-  const guildPrefix = (guild && guild.prefix) || '!'
+    const [prefix, _command, ...args] = parts
+    const context = message
+    const command = Object.keys(COMMAND_NAME).find((name) => {
+      const config = COMMANDS[name]
+      const match = (_command || '').toLowerCase()
+      return (
+        config.command === match ||
+        (config.aliases && config.aliases.includes(match))
+      )
+    })
 
-  if (prefixed && prefix !== guildPrefix) {
-    return
-  }
-
-  if (COMMANDS[command].modOnly) {
-    const mods = guild && guild.mods ? Object.keys(guild.mods) : []
-    const authorId = message.author.id
-    const guildMember = await context.guild.members.fetch(authorId)
-    const isAdmin =
-      guildMember.hasPermission(['ADMINISTRATOR']) ||
-      mods.indexOf(authorId) > -1
-
-    if (!isAdmin) {
-      message.channel.send(ERRORS.MOD_ONLY)
+    if (!command) {
+      if (!prefixed) {
+        message.channel.send(
+          'Sorry, I didn\'t understand that command. Try "@LeagueBot help" for more info.'
+        )
+      }
       return
     }
-  }
 
-  try {
-    console.log('Command received!', command, args)
+    const guild = await guilds.get(message.guild.id)
+    const guildPrefix = (guild && guild.prefix) || '!'
+
+    if (prefixed && prefix !== guildPrefix) {
+      return
+    }
+
+    if (COMMANDS[command].modOnly) {
+      const mods = guild && guild.mods ? Object.keys(guild.mods) : []
+      const authorId = message.author.id
+      const guildMember = await context.guild.members.fetch(authorId)
+      const isAdmin =
+        guildMember.hasPermission(['ADMINISTRATOR']) ||
+        mods.indexOf(authorId) > -1
+
+      if (!isAdmin) {
+        message.channel.send(ERRORS.MOD_ONLY)
+        return
+      }
+    }
+
+    console.log(`[${message.guild.name}] Command received!`, command, args)
 
     const guildUpdate = {
       id: message.guild.id,
